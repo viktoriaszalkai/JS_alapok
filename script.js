52b33c911098cc6497246c3cffa3adc9e303fd3f
csefikatalin <csefi.katalin@gmail.com>
2023-03-15 21:41:53 +0100

    feladatok

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+    elemekElerese1();
+    elemekElerese2();
+    elemekElerese3();
+});
+
+function elemekElerese1() {
+    /**Ide jön az első feladat */
+}
+function elemekElerese1() {
+    /**Ide jön az 2.  feladat */
+}
+function elemekElerese1() {
+    /**Ide jön az 3. feladat */
+}
