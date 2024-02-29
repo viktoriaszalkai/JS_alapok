c080dc04d9239f50c495817db26d4a474ca43491
csefikatalin <62071696+csefikatalin@users.noreply.github.com>
2024-02-29 11:46:42 +0100

    script.js jav

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ window.addEventListener("load", function () {
 function elemekElerese1() {
     /**Ide jön az első feladat */
 }
-function elemekElerese1() {
+function elemekElerese2() {
     /**Ide jön az 2.  feladat */
 }
-function elemekElerese1() {
+function elemekElerese3() {
     /**Ide jön az 3. feladat */
 }
