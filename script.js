513f433102e50a308198f1cfbe0f671246fddb68
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2024-03-01 11:41:44 +0100

    elemek formazasa 1

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 window.addEventListener("load", function () {
-    elemekElerese1();
-    elemekElerese2();
-    elemekElerese3();
+  elemekElerese1();
+  elemekElerese2();
+  elemekElerese3();
+  elemekElerese4();
+  elemekFormazasa1();
 });
 
 function elemekElerese1() {
-    /**Ide jön az első feladat */
+  const ELEM = document.querySelectorAll("section h2")[0];
+  console.log(ELEM.innerHTML);
 }
 function elemekElerese2() {
-    /**Ide jön az 2.  feladat */
+  const ELEM = document.querySelectorAll("#ide")[0];
+  ELEM.innerHTML = "<p>Jó reggelt!</p>";
 }
 function elemekElerese3() {
-    /**Ide jön az 3. feladat */
+  const ELEM = document.querySelectorAll(".ide")[0];
+  ELEM.innerHTML = "<p>Jó reggelt!</p>";
 }
+
+function elemekElerese4() {
+  const LISTA = document.querySelectorAll(".lista");
+
+  let txt = "<ul>";
+  for (let i = 0; i < 5; i++) {
+    txt += `<li>${Math.floor(Math.random() * 20 + 10)}</li>`;
+  }
+  txt += "</ul>";
+  console.log(txt);
+  LISTA[0].innerHTML = txt
+  return LISTA
+}
+
+function elemekFormazasa1(){
+    const LISTA2 = document.querySelectorAll(".lista"); 
+    LISTA2[0].classList.add("formazott")   
+}
+
